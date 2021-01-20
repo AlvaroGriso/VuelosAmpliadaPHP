91ba02d4dd2290b12c12032758a0bea2fe9228d1
Alvaro Grisolia Vaquero <57708608+AlvaroGriso@users.noreply.github.com>
2021-01-20 14:20:19 +0100

    Add files via upload

--- comprar.php
+++ comprar.php
@@ -1,10 +1,19 @@
 <?php
-
 require 'vendor/autoload.php'; // include Composer goodies
 
+
+
+$recibido = file_get_contents('php://input');
+$primerCaracter = substr($recibido,0,1);
+if($primerCaracter == '{'){
+    $_GET =json_decode($recibido,true);
+
+}else{
+    parse_str($recibido,$_GET);
+}
 try {
     $cliente = new MongoDB\Client("mongodb://localhost:27017");
-    $coleccion = $cliente->VuelosAmpliado->vuelos2_0;
+    $coleccion = $cliente->VuelosAmpliada->vuelos2_0;
 
     $codigoVuelo = $_GET['codigo'];
     $dni = $_GET['dni'];
@@ -14,9 +23,9 @@ try {
     $tarjeta = $_GET['tarjeta'];
 
 
-  //  $coleccion->insert([ 'codigo' => $codigoVuelo, 'vendidos' => [ 'asiento' => '3', 'dni'=> $dni, 'apellido'=>$apellido, 'nombre'=>$nombre, 'dniPagador'=>$dniPagador, 'tarjeta'=> $tarjeta, 'codigoVenta'=>"Wonki" ]]);
+    //  $coleccion->insert([ 'codigo' => $codigoVuelo, 'vendidos' => [ 'asiento' => '3', 'dni'=> $dni, 'apellido'=>$apellido, 'nombre'=>$nombre, 'dniPagador'=>$dniPagador, 'tarjeta'=> $tarjeta, 'codigoVenta'=>"Wonki" ]]);
     //$coleccion->update(array('codigo' => $codigoVuelo),
-      //             array('$push' => array('questions' => array('vendidos' => '2'))));
+    //             array('$push' => array('questions' => array('vendidos' => '2'))));
 
     $codigoVenta = RandomCodigoVenta();
     $costeBillete = (int)RandomPrecioBillete();
@@ -24,7 +33,7 @@ try {
     $query = array('codigo' => $codigoVuelo);
     $resultado = $coleccion->find($query);
     foreach ($resultado->toArray() as $vuelo) {
-      $numAsiento = sizeof($vuelo["vendidos"]);
+        $numAsiento = sizeof($vuelo["vendidos"]);
     }
 
     $nuevoAsiento = array('asiento' => $numAsiento + 1, 'dni' => $dni, 'apellido' => $apellido, 'nombre' => $nombre, 'dniPagador' => $dniPagador, 'tarjeta' => $tarjeta, 'codigoVenta' => $codigoVenta, 'costeBillette' => $costeBillete);
@@ -35,41 +44,35 @@ try {
     $estado = false;
     $estado = true;
 
-    $count = 0;
-    $list_vuelos[] = array();
+
+$list_vuelos [] = [];
 
     foreach ($resultado->toArray() as $vuelo) {
-      $vendidos = $vuelo["vendidos"][$numAsiento];
-        $array = array(
+        $vendidos = $vuelo["vendidos"][$numAsiento];
+        $list_vuelos = array(
+            "estado" => $estado,
             "codigo" => $vuelo["codigo"],
             "origen" => $vuelo["origen"],
             "destino" => $vuelo["destino"],
             "fecha" => $vuelo["fecha"],
             "hora" => $vuelo["hora"],
-            array(
-            "asiento" => $vendidos["asiento"],
-            "dni" => $vendidos["dni"],
-            "apellido" => $vendidos["apellido"],
-            "nombre" => $vendidos["nombre"],
-            "dniPagador" => $vendidos["dniPagador"],
-            "tarjeta" => $vendidos ["tarjeta"],
-            "codigoVenta" => $vendidos["codigoVenta"],
-            "costeBillette" => $vendidos["costeBillette"]
-          )
+                "asiento" => $vendidos["asiento"],
+                "dni" => $vendidos["dni"],
+                "apellido" => $vendidos["apellido"],
+                "nombre" => $vendidos["nombre"],
+                "dniPagador" => $vendidos["dniPagador"],
+                "tarjeta" => $vendidos ["tarjeta"],
+                "codigoVenta" => $vendidos["codigoVenta"],
+                "costeBillette" => $vendidos["costeBillette"]
         );
 
-        $list_vuelos[$count] = $array;
 
-        $count++;
     }
 
-    $arrayFinal = array(
-        "estado" => $estado,
-        "vuelos" => $list_vuelos
-    );
 
-    header("Content-type:application/json");
-    echo json_encode($arrayFinal);
+
+    $miVariable = json_encode($list_vuelos, JSON_PRETTY_PRINT);
+    echo $miVariable;
 
 } catch (MongoDB\Driver\Exception\Exception $e) {
     print_r($e);
@@ -80,7 +83,7 @@ function RandomCodigoVenta(){
     $charactersLength = strlen($characters);
     $randomCodigoVenta = '';
     for ($i = 0; $i < 9; $i++) {
-      $randomCodigoVenta .= $characters[rand(0, $charactersLength - 1)];
+        $randomCodigoVenta .= $characters[rand(0, $charactersLength - 1)];
     }
     return $randomCodigoVenta;
 }
@@ -90,7 +93,11 @@ function RandomPrecioBillete(){
     $charactersLength = strlen($characters);
     $randomPrecio = '';
     for ($i = 0; $i < 3; $i++) {
-      $randomPrecio .= $characters[rand(0, $charactersLength - 1)];
+        $randomPrecio .= $characters[rand(0, $charactersLength - 1)];
     }
     return $randomPrecio;
 }
+
+
+?>
+
